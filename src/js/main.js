c113ecd635e40e43ab8ba90eeabee8ff23269771
Justin.Akdogan <justin.akdogan@gmail.com>
2019-01-15 16:24:03 +0100

    New Images added Google Maps included New Functions in JavaScript

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,10 @@ var counter = 0;
 var searchParams = new URLSearchParams(window.location.search);
 myHeadline = document.getElementById('maincontent_headline');
 
-
-
-
 //Counter Functionality for Images
 $("#next").click(function(){
     counter++;
+    console.log('NEXT: ' + counter)
     if(counter > 5) {
         counter = 0;
     }
@@ -16,23 +14,28 @@ $("#next").click(function(){
 });
 $("#back").click(function(){
     counter--;
-    if(counter <= 0) {
+    console.log('BACK: ' + counter)
+    if(counter < 0) {
         counter = 5;
     }
     document.getElementById('images').src = 'src/img/apartments_' + counter + '.jpg';
 });
+
+
 
 //Show the Pagerelated elements and hide other Elements
 if(searchParams.has('page')){
    if (searchParams.get('page') == 'approach'){
        hideAll();
         document.getElementById('gmaps').style.display = 'block';
+        document.getElementById('menuItem2').style.borderBottom = '0.180em solid ghostwhite';
         myHeadline.innerText = 'Unser Standort';
    }
 }else{
     document.getElementById('images').style.display = 'block';
     document.getElementById('back').style.display = 'block';
     document.getElementById('next').style.display = 'block';
+    document.getElementById('menuItem1').style.borderBottom = '0.180em solid ghostwhite';
 }
 
 function hideAll(){
@@ -41,3 +44,5 @@ function hideAll(){
     document.getElementById('next').style.display = 'none';
     document.getElementById('gmaps').style.display = 'none';
 }
+
+
